c2feb5b29c723bf35a059cde171181d8df9267b3
Adeolaleye <46665612+Adeolaleye@users.noreply.github.com>
2021-10-20 08:39:05 +0100

    complete application

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('home');
+// });
+
+Route::get('/', 'ContestantCatController@index');
+Route::get('/home', 'ContestantCatController@index')->name('home');
+
+Route::get('vote', function() {
+    return view('vote');
+});
+Route::resource('contestant', 'ContestantCatController');
+Route::Post('votecontestant', 'ContestantCatController@vote')->name('vote4contestant');
+Route::view('feedback','admin.login'); 
+Route::resource('sitreservation','ReserveController');
+Route::Post('sitreservation', 'ReserveController@store')->name('ticket');
+//Route::view('yourticket','bookings.ticket');
+
+Auth::routes();
+Route::middleware(['auth'])->group (function() {
+    Route::view('addcategory','admin.addcategory');
+    Route::Post('addcategories','adminController@store')->name('addcat');
+    Route::get('addnominees','adminController@list');
+    Route::Post('addnominees','adminController@saveContestant')->name('addnorm');
+    Route::resource('result','adminController');
+    Route::Post('delete', 'adminController@destroy')->name('deletecat');
+});
